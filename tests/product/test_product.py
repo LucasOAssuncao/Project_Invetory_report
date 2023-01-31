57c4c985291a2d01ca6caa94a8eae12d9b2ffd90
Lucas Otoni <lotoni.vivo@gmail.com>
2023-01-31 15:01:59 -0300

    feat: test_product implementada

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product /
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    created_product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        1551,
+        "instrucao 1"
+    )
+
+    assert created_product.id == 1
+    assert created_product.nome_do_produto == "Nicotine Polacrilex"
+    assert created_product.nome_da_empresa == "Target Corporation"
+    assert created_product.data_de_fabricacao == "2021-02-18"
+    assert created_product.data_de_validade == "2023-09-17"
+    assert created_product.numero_de_serie == 1551
+    assert created_product.instrucoes_de_armazenamento == "instrucao 1"
