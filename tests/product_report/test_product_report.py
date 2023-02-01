e6e793ac25c8bec1e14baf1909b55c907452ce99
Lucas Otoni <lotoni.vivo@gmail.com>
2023-02-01 14:41:11 -0300

    feat: add product_report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        1551,
+        "instrucao 1",
+    )
+    product_report = (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert repr(product) == product_report
